d9d51bdc840c2fd9e033dfd0c449d581163565a4
Serhiy Mytrovtsiy <mitrovtsiy@ukr.net>
2020-01-22 23:14:57 +0100

    remove Carthage vendor folder;

--- Carthage/Checkouts/Charts/Gemfile
+++
@@ -1,6 +0,0 @@
-source 'https://rubygems.org'
-
-gem 'cocoapods', '>= 1.6.0.beta.1'
-gem 'github_changelog_generator'
-gem 'rake'
-gem 'xcpretty'
--- Carthage/Checkouts/LaunchAtLogin/LaunchAtLogin/LaunchAtLogin.h
+++
@@ -1,4 +0,0 @@
-#import <Cocoa/Cocoa.h>
-
-FOUNDATION_EXPORT double LaunchAtLoginVersionNumber;
-FOUNDATION_EXPORT const unsigned char LaunchAtLoginVersionString[];
