c39b91c0b8e9ed52e1d9953281dabc206cff632c
Serhiy Mytrovtsiy <mitrovtsiy@ukr.net>
2020-10-31 21:13:31 +0100

    - fix Battery module settings visibility - add option to select the battery time: short (01:25) and long (1h 25min) (#129) - add missing translations

--- StatsKit/extensions.swift
+++ StatsKit/extensions.swift
@@ -187,34 +187,26 @@ public extension Double {
         }
     }
     
-    func secondsToHoursMinutesSeconds() -> (Int?, Int?, Int?) {
-        let hrs = self / 3600
+    func secondsToHoursMinutesSeconds() -> (Int, Int) {
         let mins = (self.truncatingRemainder(dividingBy: 3600)) / 60
-        let seconds = (self.truncatingRemainder(dividingBy:3600)).truncatingRemainder(dividingBy:60)
-        return (Int(hrs) > 0 ? Int(hrs) : nil , Int(mins) > 0 ? Int(mins) : nil, Int(seconds) > 0 ? Int(seconds) : nil)
+        return (Int(self / 3600) , Int(mins))
     }
     
-    func printSecondsToHoursMinutesSeconds() -> String {
-        let time = self.secondsToHoursMinutesSeconds()
+    func printSecondsToHoursMinutesSeconds(short: Bool = false) -> String {
+        let (h, m) = self.secondsToHoursMinutesSeconds()
+        let minutes = m > 9 ? "\(m)" : "0\(m)"
         
-        switch time {
-        case (nil, let x? , let y?):
-            return "\(x)min \(y)sec"
-        case (nil, let x?, nil):
-            return "\(x)min"
-        case (let x?, nil, nil):
-            return "\(x)h"
-        case (nil, nil, let x?):
-            return "\(x)sec"
-        case (let x?, nil, let z?):
-            return "\(x)h \(z)sec"
-        case (let x?, let y?, nil):
-            return "\(x)h \(y)min"
-        case (let x?, let y?, let z?):
-            return "\(x)h \(y)min \(z)sec"
-        default:
-            return "n/a"
+        if short {
+            return "\(h):\(minutes)"
         }
+        
+        if h == 0 {
+            return "\(minutes)min"
+        } else if m == 0 {
+            return "\(h)h"
+        }
+        
+        return "\(h)h \(minutes)min"
     }
 }
 
