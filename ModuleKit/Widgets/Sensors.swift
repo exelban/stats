bc4bdf904325cf7111af0011d6f0e5d5ef051e2f
Serhiy Mytrovtsiy <mitrovtsiy@ukr.net>
2020-11-16 19:50:13 +0100

    - fix Sensors updates in one of the screens in multi-monitor configuration (#168)

--- ModuleKit/Widgets/Sensors.swift
+++ ModuleKit/Widgets/Sensors.swift
@@ -202,6 +202,7 @@ public class SensorsWidget: Widget {
                         }
                     }
                 }
+                self.needsDisplay = true
             }
         })
     }
