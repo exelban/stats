e53533504d79c0aa61fb4852113f102a0b70e22b
NanoSector <rick@nanosector.nl>
2020-11-20 21:43:25 +0100

    Battery: Refactor processCallback method
    Signed-off-by: NanoSector <rick@nanosector.nl>

--- Modules/Battery/popup.swift
+++ Modules/Battery/popup.swift
@@ -254,20 +254,24 @@ internal class Popup: NSView, Popup_p {
     
     public func processCallback(_ list: [TopProcess]) {
         DispatchQueue.main.async(execute: {
-            if (self.window?.isVisible ?? false) || !self.processesInitialized {
-                for i in 0..<list.count {
-                    let process = list[i]
-                    let index = list.count-i-1
-                    if self.processes.indices.contains(index) {
-                        self.processes[index].label = process.name != nil ? process.name! : process.command
-                        self.processes[index].value = "\(process.usage)%"
-                        self.processes[index].icon = process.icon
-                        self.processes[index].toolTip = "pid: \(process.pid)"
-                    }
-                }
-                
-                self.processesInitialized = true
+            if !(self.window?.isVisible ?? false) && self.processesInitialized {
+                return;
             }
+            
+            if list.count != self.processes.count {
+                self.processes.forEach { processView in
+                    processView.clear();
+                }
+            }
+            
+            for i in 0..<list.count {
+                let process = list[i]
+                let index = list.count-i-1
+                self.processes[index].attachProcess(process);
+                self.processes[index].value = "\(process.usage)%"
+            }
+            
+            self.processesInitialized = true
         })
     }
 }
