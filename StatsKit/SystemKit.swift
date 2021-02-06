98c9dc26adca8fc5cd10ac7eaf0b536afffa3926
Tai Zhou <21986946+Tai-Zhou@users.noreply.github.com>
2021-02-06 13:47:39 +0800

    Update macOS Name Recognition

--- StatsKit/SystemKit.swift
+++ StatsKit/SystemKit.swift
@@ -115,7 +115,7 @@ public class SystemKit {
             build = buildArr[1].replacingOccurrences(of: "Build ", with: "").replacingOccurrences(of: ")", with: "")
         }
         
-        let version = "\(systemVersion.majorVersion).\(systemVersion.minorVersion)"
+        let version = systemVersion.majorVersion > 10 ? "\(systemVersion.majorVersion)" : "\(systemVersion.majorVersion).\(systemVersion.minorVersion)"
         self.device.os = os_s(name: osDict[version] ?? LocalizedString("Unknown"), version: systemVersion, build: build)
         
         self.device.info.cpu = self.getCPUInfo()
@@ -467,6 +467,5 @@ let deviceDict: [String: model_s] = [
 let osDict: [String: String] = [
     "10.14": "Mojave",
     "10.15": "Catalina",
-    "11.0": "Big Sur",
-    "11.1": "Big Sur",
+    "11": "Big Sur",
 ]
