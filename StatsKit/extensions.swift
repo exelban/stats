ad21440eb39fc0233c6be83be1ed848e163c77f9
Serhiy Mytrovtsiy <mitrovtsiy@ukr.net>
2021-03-17 20:34:17 +0100

    fix: allow to copy value from NSTextView by CMD+C (#383)

--- StatsKit/extensions.swift
+++ StatsKit/extensions.swift
@@ -559,3 +559,34 @@ public final class ScrollableStackView: NSView {
         fatalError("init(coder:) has not been implemented")
     }
 }
+
+// https://stackoverflow.com/a/54492165
+extension NSTextView {
+    override open func performKeyEquivalent(with event: NSEvent) -> Bool {
+        let commandKey = NSEvent.ModifierFlags.command.rawValue
+        let commandShiftKey = NSEvent.ModifierFlags.command.rawValue | NSEvent.ModifierFlags.shift.rawValue
+        if event.type == NSEvent.EventType.keyDown {
+            if (event.modifierFlags.rawValue & NSEvent.ModifierFlags.deviceIndependentFlagsMask.rawValue) == commandKey {
+                switch event.charactersIgnoringModifiers! {
+                case "x":
+                    if NSApp.sendAction(#selector(NSText.cut(_:)), to:nil, from:self) { return true }
+                case "c":
+                    if NSApp.sendAction(#selector(NSText.copy(_:)), to:nil, from:self) { return true }
+                case "v":
+                    if NSApp.sendAction(#selector(NSText.paste(_:)), to:nil, from:self) { return true }
+                case "z":
+                    if NSApp.sendAction(Selector(("undo:")), to:nil, from:self) { return true }
+                case "a":
+                    if NSApp.sendAction(#selector(NSResponder.selectAll(_:)), to:nil, from:self) { return true }
+                default:
+                    break
+                }
+            } else if (event.modifierFlags.rawValue & NSEvent.ModifierFlags.deviceIndependentFlagsMask.rawValue) == commandShiftKey {
+                if event.charactersIgnoringModifiers == "Z" {
+                    if NSApp.sendAction(Selector(("redo:")), to:nil, from:self) { return true }
+                }
+            }
+        }
+        return super.performKeyEquivalent(with: event)
+    }
+}
