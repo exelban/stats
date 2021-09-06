9193b49c1f6971ae7868646b67b1568f3a42c330
Serhiy Mytrovtsiy <mitrovtsiy@ukr.net>
2021-09-06 20:25:23 +0200

    feat: added average CPU load reader

--- Modules/CPU/readers.swift
+++ Modules/CPU/readers.swift
@@ -455,3 +455,49 @@ public class LimitReader: Reader<CPU_Limit> {
         self.callback(self.limits)
     }
 }
+
+public class AverageReader: Reader<[Double]> {
+    private let title: String = "CPU"
+    
+    public override func setup() {
+        self.setInterval(60)
+    }
+    
+    public override func read() {
+        let task = Process()
+        task.launchPath = "/usr/bin/uptime"
+        
+        let outputPipe = Pipe()
+        defer {
+            outputPipe.fileHandleForReading.closeFile()
+        }
+        task.standardOutput = outputPipe
+        
+        do {
+            try task.run()
+        } catch let err {
+            error("error read uptime: \(err.localizedDescription)", log: self.log)
+            return
+        }
+        
+        let outputData = outputPipe.fileHandleForReading.readDataToEndOfFile()
+        guard let line = String(decoding: outputData, as: UTF8.self).split(separator: "\n").first else {
+            return
+        }
+        
+        var str = line.trimmingCharacters(in: .whitespaces)
+        let strArr = str.findAndCrop(pattern: "(\\d+(.|,)\\d+ *){3}$").split(separator: " ")
+        
+        guard strArr.count == 3 else {
+            return
+        }
+        
+        var list: [Double] = []
+        strArr.forEach { (n: Substring) in
+            let value = Double(n.replacingOccurrences(of: ",", with: ".")) ?? 0
+            list.append(value)
+        }
+        
+        self.callback(list)
+    }
+}
