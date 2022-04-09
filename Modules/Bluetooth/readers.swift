f55bcff535cccf205a4891c95dbd270385a05244
Serhiy Mytrovtsiy <mitrovtsiy@ukr.net>
2022-04-09 11:50:56 +0200

    fix: enabled new BLE battery level reader only for macOS 11+

--- Modules/Bluetooth/readers.swift
+++ Modules/Bluetooth/readers.swift
@@ -225,6 +225,8 @@ internal class DevicesReader: Reader<[BLEDevice]>, CBCentralManagerDelegate, CBP
     // MARK: - system_profiler
     
     private func profilerDevices() -> ([bleDevice], [String]) {
+        if #available(macOS 11, *) {} else { return ([], []) }
+        
         guard let res = process(path: "/usr/sbin/system_profiler", arguments: ["SPBluetoothDataType", "-json"]) else {
             return ([], [])
         }
