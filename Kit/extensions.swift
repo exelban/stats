ac084bca2858b39663ed1f66b88eac18aa6dae27
Serhiy Mytrovtsiy <mitrovtsiy@ukr.net>
2023-04-04 18:56:10 +0200

    feat: added missing time zones to the clock

--- Kit/extensions.swift
+++ Kit/extensions.swift
@@ -602,7 +602,30 @@ public extension Date {
 
 public extension TimeZone {
     init(fromUTC: String) {
-        if let utc = Int(fromUTC), let tz = TimeZone(secondsFromGMT: utc*3600) {
+        if fromUTC == "local" {
+            self = TimeZone.current
+            return
+        }
+        
+        let arr = fromUTC.split(separator: ":")
+        guard !arr.isEmpty else {
+            self = TimeZone.current
+            return
+        }
+        
+        var secondsFromGMT = 0
+        if arr.indices.contains(0), let h = Int(arr[0]) {
+            secondsFromGMT += h*3600
+        }
+        if arr.indices.contains(1), let m = Int(arr[1]) {
+            if secondsFromGMT < 0 {
+                secondsFromGMT -= m*60
+            } else {
+                secondsFromGMT += m*60
+            }
+        }
+        
+        if let tz = TimeZone(secondsFromGMT: secondsFromGMT) {
             self = tz
         } else {
             self = TimeZone.current
