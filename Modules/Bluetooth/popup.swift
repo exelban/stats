45d571e19f98494cef1a8d65739347f116b2a0ac
Serhiy Mytrovtsiy <mitrovtsiy@ukr.net>
2024-08-26 18:26:20 +0200

    feat: adjusted bluetooth device margins in the popup view (#2092)

--- Modules/Bluetooth/popup.swift
+++ Modules/Bluetooth/popup.swift
@@ -80,6 +80,7 @@ internal class BLEView: NSStackView {
         self.alignment = .centerY
         self.spacing = 0
         self.wantsLayer = true
+        self.edgeInsets = NSEdgeInsets(top: 0, left: 5, bottom: 0, right: 5)
         self.layer?.cornerRadius = 2
         
         let nameView: NSTextField = TextView(frame: NSRect(x: 0, y: 0, width: 0, height: 16))
