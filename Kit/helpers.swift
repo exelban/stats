3c6b954f67be62d8cd2efba048283d489a66f861
Yohaan Narayanan <229655281+yohaann196@users.noreply.github.com>
2026-04-02 22:15:15 -0700

    Adjust width constraints in helpers.swift
    Signed-off-by: Yohaan Narayanan <229655281+yohaann196@users.noreply.github.com>

--- Kit/helpers.swift
+++ Kit/helpers.swift
@@ -1540,6 +1540,7 @@ public class StepperInput: NSStackView, NSTextFieldDelegate, PreferencesSwitchWi
         self.valueView.delegate = self
         self.valueView.stringValue = "\(value)"
         self.valueView.translatesAutoresizingMaskIntoConstraints = false
+        self.valueView.widthAnchor.constraint(greaterThanOrEqualToConstant: 35).isActive = true
         
         self.stepperView.font = NSFont.systemFont(ofSize: 12, weight: .regular)
         self.stepperView.doubleValue = Double(value)/100
@@ -1555,7 +1556,7 @@ public class StepperInput: NSStackView, NSTextFieldDelegate, PreferencesSwitchWi
         
         if units == nil {
             if unit == "%" {
-                self.widthAnchor.constraint(equalToConstant: 68).isActive = true
+                self.widthAnchor.constraint(equalToConstant: 80).isActive = true
             }
             if visibileUnit {
                 let symbol: NSTextField = LabelField(unit)
